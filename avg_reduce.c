1092e956124e71ef502bb582ce302b912fdf7eea
Ritvik-Gupta <ritvikg9@gmail.com>
2022-08-25 20:49:00 +0530

    Created Lab DA 4 MPI Collective Communication algorithms

---
+++ avg_reduce.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <mpi.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define DATASET_SIZE 100000000
+
+void init_dataset(double dataset[]) {
+	srand(time(NULL));
+	for (int i = 0; i < DATASET_SIZE; ++i)
+		dataset[i] = (double)(rand() % 1000) / 1000.0;
+}
+
+double compute_average(double dataset[], int size) {
+	double total = 0.0;
+	for (int i = 0; i < size; ++i)
+		total += dataset[i];
+	return total / (double)size;
+}
+
+void main(int argc, char* argv[]) {
+	int rank, world_size;
+	double start_stamp, end_stamp;
+	
+	MPI_Init(&argc, &argv);
+	MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+	double* dataset;
+	if (rank == 0) {
+		dataset = (double*) calloc(DATASET_SIZE, sizeof(double));
+		init_dataset(dataset);
+	} 
+
+	start_stamp = MPI_Wtime();
+
+	int elms_per_process = DATASET_SIZE / world_size;
+	double* subset = (double*) calloc(elms_per_process, sizeof(double));
+	MPI_Scatter(dataset, elms_per_process, MPI_DOUBLE, subset, elms_per_process, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+	double sub_average = compute_average(subset, elms_per_process);
+
+	double overall_average = 0.0;
+	MPI_Reduce(&sub_average, &overall_average, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+	overall_average /= world_size;
+
+	if (rank == 0) {
+		end_stamp = MPI_Wtime();
+		printf("Distributed Reduced Average = %.15g\n\ttook %.15g secs\n", overall_average, end_stamp - start_stamp);
+	
+		start_stamp = MPI_Wtime();
+		overall_average = compute_average(dataset, DATASET_SIZE);
+		end_stamp = MPI_Wtime();
+		printf("Sequential Average          = %.15g\n\ttook %.15g secs\n", overall_average, end_stamp - start_stamp);
+	}
+
+	MPI_Finalize();
+}
+
---
+++ freq.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <mpi.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define DATASET_SIZE 1000000
+
+void init_dataset(int dataset[], int* elm) {
+	srand(time(NULL));
+	
+	for (int i = 0; i < DATASET_SIZE; ++i)
+		dataset[i] = rand() % 100;
+
+	*elm = rand() % 100;
+}
+
+int compute_frequency(int dataset[], int size, int elm) {
+	int freq = 0;
+	for (int i = 0; i < size; ++i)
+		freq += dataset[i] == elm;
+	return freq;
+}
+
+void main(int argc, char* argv[]) {
+	int rank, world_size;
+	double start_stamp, end_stamp;
+
+	MPI_Init(&argc, &argv);
+	MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+	int* dataset;
+	int freq_of_elm;
+	if (rank == 0) {
+		dataset = (int*) calloc(DATASET_SIZE, sizeof(int));
+		init_dataset(dataset, &freq_of_elm);
+	}
+
+	start_stamp = MPI_Wtime();
+	int elms_per_process = DATASET_SIZE / world_size;
+
+	int* subset = (int*) calloc(elms_per_process, sizeof(int)); 
+	MPI_Scatter(dataset, elms_per_process, MPI_INT, subset, elms_per_process, MPI_INT, 0, MPI_COMM_WORLD);
+	MPI_Bcast(&freq_of_elm, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+	int freq = compute_frequency(subset, elms_per_process, freq_of_elm);
+	int total_freq = 0;
+	MPI_Reduce(&freq, &total_freq, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+
+	if (rank == 0) {
+		end_stamp = MPI_Wtime();
+		printf("Distributed Reduced Frequency of %d = %d\n\ttook %.15g\n", freq_of_elm, total_freq, end_stamp - start_stamp);
+
+		start_stamp = MPI_Wtime();
+		total_freq = compute_frequency(dataset, DATASET_SIZE, freq_of_elm);
+		end_stamp = MPI_Wtime();
+		printf("Sequential Frequency of          %d = %d\n\ttook %.15g\n", freq_of_elm, total_freq, end_stamp - start_stamp);
+	}
+
+	MPI_Finalize();
+}
